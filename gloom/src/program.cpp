21add7a383d6f6a8fa0aa2b54d6f142d53e94e9c
sigtot <sigurdtotland@gmail.com>
2019-08-25 13:33:51 +0200

    Use vectors instead of C arrays and draw 5 triangles

--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -1,11 +1,12 @@
 // Local headers
 #include <gloom/shader.hpp>
+#include <vector>
 #include "program.hpp"
 #include "gloom/gloom.hpp"
 
 #define NUM_COORDINATES 3
 
-unsigned int createTriangleVAO(float* vertices, unsigned int* indices, int numPoints)
+unsigned int createVAO(std::vector<float> vertices, std::vector<unsigned int> indices, unsigned int numPoints)
 {
     unsigned int VAO = 0;
     glGenVertexArrays(1, &VAO);
@@ -14,7 +15,7 @@ unsigned int createTriangleVAO(float* vertices, unsigned int* indices, int numPo
     unsigned int VBO = 0;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, NUM_COORDINATES * numPoints * sizeof(float), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, NUM_COORDINATES * numPoints * sizeof(float), &vertices[0], GL_STATIC_DRAW);
 
     glVertexAttribPointer(
             0,
@@ -29,7 +30,7 @@ unsigned int createTriangleVAO(float* vertices, unsigned int* indices, int numPo
     unsigned int IBO = 0;
     glGenBuffers(1, &IBO );
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, numPoints * NUM_COORDINATES * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, numPoints * NUM_COORDINATES * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
     return VAO;
 }
 
@@ -47,14 +48,22 @@ void runProgram(GLFWwindow* window)
 
     // Set up scene
     Gloom::Shader shader;
-    shader.makeBasicShader("../gloom/shaders/simple.vert",
-                           "../gloom/shaders/simple.frag");
-    // A nice triangle
-    //float vertices[9] =  { 0.6, 0.7, -1.2, 0.f, 0.4, 0.f, -0.8, -0.2, 1.2 };
-    float vertices[9] = {-0.3, -0.4, 0.0, 0.3, -0.1, -0.5, -0.2, 0.5, 0.7};
-    unsigned int indices[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    int numPoints = 3;
-    unsigned int VAO = createTriangleVAO(vertices, indices, numPoints);
+    // Fix these dumb paths some time
+    shader.makeBasicShader("/home/sigtot/Developer/visdat/ovs/ov1/gloom/gloom/shaders/simple.vert",
+                           "/home/sigtot/Developer/visdat/ovs/ov1/gloom/gloom/shaders/simple.frag");
+    std::vector<float> triangleCoords {
+        0.0866, 0.3, 0.0, -0.0866, 0.3, 0.0, 0.0, 0.2, 0.0,
+        0.3, -0.0866, 0.0, 0.3, 0.0866, 0.0, 0.2, 0.0, 0.0,
+        -0.0866, -0.3, 0.0, 0.0866, -0.3, 0.0, 0.0, -0.2, 0.0,
+        -0.3, 0.0866, 0.0, -0.3, -0.0866, 0.0, -0.2, 0.0, 0.0,
+        -0.0866, -0.05, 0.0, 0.0866, -0.05, 0.0, 0.0, 0.05, 0.0,
+    };
+
+    std::vector<unsigned int> indices;
+    for (unsigned long i = 0; i < triangleCoords.size(); ++i) indices.push_back(i);
+
+    unsigned int numPoints = triangleCoords.size() / NUM_COORDINATES;
+    unsigned int VAO = createVAO(triangleCoords, indices, numPoints);
 
     glPointSize(5.0f);
     glLineWidth(5.0f);
